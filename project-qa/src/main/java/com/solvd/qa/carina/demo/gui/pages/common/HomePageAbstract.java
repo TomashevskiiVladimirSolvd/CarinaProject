c552239da3a32f02eda68b805cac1a8423324b1c
VergunVira <samarskavira@gmail.com>
2023-08-04 15:10:38 -0400

    changes

--- project-qa/src/main/java/com/solvd/qa/carina/demo/gui/pages/common/HomePageAbstract.java
+++ project-qa/src/main/java/com/solvd/qa/carina/demo/gui/pages/common/HomePageAbstract.java
@@ -1,11 +1,13 @@
 package com.solvd.qa.carina.demo.gui.pages.common;
 
+import com.zebrunner.carina.webdriver.decorator.PageOpeningStrategy;
 import org.openqa.selenium.WebDriver;
 
 public abstract class HomePageAbstract extends AbstractAmazonPage {
 
     public HomePageAbstract(WebDriver driver) {
         super(driver);
+        setPageOpeningStrategy(PageOpeningStrategy.BY_ELEMENT);
     }
 
 }
--- project-qa/src/main/java/com/solvd/qa/carina/demo/gui/pages/desktop/HomePage.java
+++ project-qa/src/main/java/com/solvd/qa/carina/demo/gui/pages/desktop/HomePage.java
@@ -2,13 +2,18 @@ package com.solvd.qa.carina.demo.gui.pages.desktop;
 
 import com.solvd.qa.carina.demo.gui.pages.common.HomePageAbstract;
 import com.zebrunner.carina.utils.factory.DeviceType;
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 @DeviceType(pageType = DeviceType.Type.DESKTOP, parentClass = HomePageAbstract.class)
 public class HomePage extends HomePageAbstract {
 
+    @FindBy(xpath = "//div[@id='nav-belt']")
+    public ExtendedWebElement globalHeader;
     public HomePage(WebDriver driver) {
         super(driver);
+        setUiLoadedMarker(globalHeader);
     }
 
 }
